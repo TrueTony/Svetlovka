63361f87f713863e683e70c81577499dc34c4317
TrueTony <spanchbob@gmail.com>
2018-11-26 21:11:40 +0300

    add books in db

--- liblive/example.py
+++ liblive/example.py
@@ -6,24 +6,23 @@ f = open('list_of_books.txt', 'r')
 data = json.load(f)
 for book in data:
     for author in book[1]:
-        a = Author()
-        a.name = author
-        a.save()
+        if not Author.objects.filter(name=author).exists():
+                a = Author()
+                a.name = author
+                a.save()
 f.close()
 
 f = open('list_of_books.txt', 'r')
 data = json.load(f)
-for i in data[:5]:
-    if len(i[1]) == 1:
+for i in data:
         b = BookFromLivelib()
         b.title = i[2]
-        for name in i[1]:
-            # b.author = name
-            b.author = Author.objects.get(name=name)
+        b.author = Author.objects.get(name=i[1][0])
         b.tags = i[3]
         b.cover = i[4]
         b.rating = i[5]
         b.description = i[6]
         b.key = i[7]
         b.save()
+        
 f.close()

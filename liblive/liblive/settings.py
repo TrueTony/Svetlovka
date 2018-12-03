bcd5c25ea1c49582aa001299633b760fa44f76b2
TrueTony <spanchbob@gmail.com>
2018-12-03 20:18:51 +0300

    add register

--- liblive/liblive/settings.py
+++ liblive/liblive/settings.py
@@ -31,9 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig'
     'liv.apps.LivConfig',
+    'users.apps.UsersConfig',    
     'polls.apps.PollsConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

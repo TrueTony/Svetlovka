33e37b10c68c6dff09c383b15e34199e5e2ee60d
TrueTony <spanchbob@gmail.com>
2018-12-03 23:04:58 +0300

    upd profile

--- liblive/users/forms.py
+++ liblive/users/forms.py
@@ -5,7 +5,17 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    link = forms.CharField(max_length=200)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    link = forms.CharField(max_length=200)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'link']
